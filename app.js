1a3f898c9fb923ff012f1efaf01b6aeffe1e33d2
Fenn Ignatius Saji <fennig1997@gmail.com>
2018-03-26 15:19:12 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const socketIO = require('socket.io');
+// const socketIO = require('socket.io');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
